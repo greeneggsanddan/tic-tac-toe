61fb494da8a158dc1e28e09ff4dd979815ebd6fa
greeneggsanddan <greeneggsanddan@gmail.com>
2023-06-06 16:33:51 -0700

    Add win checking functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,5 @@
-const createPlayer = (name, sign) => {
-    return {name, sign}
+const createPlayer = (name, mark) => {
+    return {name, mark}
 }
 
 const createCell = () => {
@@ -16,6 +16,8 @@ const createCell = () => {
 
 const gameBoard = (() => {
     const board = [];
+    const playerOneMarks = [];  //Do I want to keep an array of the marks of players?
+    const playerTwoMarks = [];
 
     for (let i = 0; i < 9; i++) {
         const cell = createCell();
@@ -24,18 +26,24 @@ const gameBoard = (() => {
 
     const getBoard = () => board;
 
-    const markCell = (position, sign) => {
-        board[position].addMark(sign);
+    const markCell = (position, mark) => {
+        board[position].addMark(mark);
     }
 
     return {getBoard, markCell};
 })();
 
 const gameController = ((playerOneName = "Player One", playerTwoName = "Player Two") => {
-    //const board = gameBoard;    //do I have to declare game as a variable or can I just use gameBoard?
     const playerOne = createPlayer("Player 1", "X");
     const playerTwo = createPlayer("Player 2", "O");
     let activePlayer = playerOne;
+    let roundNumber = 0;
+    let gameOver = false;
+    const winners = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],    //Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],    //Columns
+        [0, 4, 8], [2, 4, 6]                //Diagonals
+    ];
 
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
@@ -43,18 +51,35 @@ const gameController = ((playerOneName = "Player One", playerTwoName = "Player T
 
     const getActivePlayer = () => activePlayer;
 
-    const playRound = (position) => {
-        gameBoard.markCell(position, getActivePlayer().sign);   //Check if this needs to be a variable
-        //Check for winner
-        switchPlayerTurn();
-        //Update the display
+    const getRound = () => roundNumber;
+
+    const checkForWinner = (board, mark) => {
+        let isWinner = false;
+        const marked = []; //Do I want make a new array every time a move is made?
+        for (let i = 0; i < 9; i++) {
+            if (board[i].getValue() === mark) marked.push(i);
+        }
+        console.log(marked);
+        for (let i = 0; i < winners.length; i++) {
+            isWinner = winners[i].every(winningPosition => marked.includes(winningPosition));
+            if (isWinner) break;
+        }
+        return isWinner;
     }
 
-    return {playRound, getActivePlayer};
+    const playRound = (position) => {
+        roundNumber++;
+        const mark = getActivePlayer().mark;
+        gameBoard.markCell(position, mark);
+        if (checkForWinner(gameBoard.getBoard(), mark) || roundNumber === 9) {
+            gameOver = true;
+        } else switchPlayerTurn();
+    }
+
+    return {playRound, getActivePlayer, checkForWinner, getRound};
 })();
 
 const displayController = (() => {
-    // const game = gameController();  //Do I need this?
     const playerTurnDiv = document.querySelector(".turn");
     const boardDiv = document.querySelector(".board");
 
@@ -62,12 +87,19 @@ const displayController = (() => {
         boardDiv.textContent = "";
         const board = gameBoard.getBoard();
         const activePlayer = gameController.getActivePlayer();
-        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+
+        if (gameController.checkForWinner(board, activePlayer.mark)) {
+            playerTurnDiv.textContent = `${activePlayer.name} wins!`;
+        } else if (gameController.getRound() === 9) {
+            playerTurnDiv.textContent = "Draw!";
+        } else {
+            playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+        }
 
         board.forEach((cell, position) => {
             const cellButton = document.createElement("button");
             cellButton.classList.add("cell");
-            cellButton.dataset.position = position; //Check if you still need this
+            cellButton.dataset.position = position; //check that this is doing the right thing
             cellButton.textContent = cell.getValue();
             boardDiv.appendChild(cellButton);
         });
